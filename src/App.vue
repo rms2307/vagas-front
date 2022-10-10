a9d638b263dcf90c693053e9136911f033b348f0
Robson de Moraes Silva <rms2307@outlook.com>
2022-10-10 19:30:52 -0300

    Add bootstrap e navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <topo-padrao> </topo-padrao>
+    <Conteudo></Conteudo>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Conteudo from "./components/layouts/ConteudoApp.vue";
+import TopoPadrao from "./components/layouts/TopoPadrao.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Conteudo,
+    TopoPadrao,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+#app {}
 </style>
---
+++ src/components/layouts/ConteudoApp.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p>Conteudo</p>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
