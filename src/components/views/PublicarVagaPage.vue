2b0d3afe62fc07b06e6a066f990f7fab60aeabd3
Robson de Moraes Silva <rms2307@outlook.com>
2022-10-23 20:36:43 -0300

    Implementando componente PublicarVagaPage e salvando vagas no LocalStorage

--- src/components/views/PublicarVagaPage.vue
+++ src/components/views/PublicarVagaPage.vue
@@ -1,12 +1,95 @@
 <template>
-  <div>
-    <h1>Componente Publicar Vaga</h1>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <h4>Apresente a sua vaga para milhares de profissionais e de graça</h4>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Título da Vaga</label>
+        <input type="text" class="form-control" v-model="titulo" />
+        <div class="form-text">
+          Por exemplo: Programador JavaScript e VuaJS.
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Descrição</label>
+        <textarea type="text" class="form-control" v-model="descricao" />
+        <div class="form-text">Informe os detalhes da vaga.</div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <label class="form-label">Salário</label>
+        <input type="number" class="form-control" v-model="salario" />
+        <div class="form-text">Informe o salário.</div>
+      </div>
+
+      <div class="col">
+        <label class="form-label">Modalidade</label>
+        <select class="form-select" v-model="modalidade">
+          <option value="" disabled>Selecione...</option>
+          <option value="1">Home Office</option>
+          <option value="2">Presencial</option>
+        </select>
+        <div class="form-text">
+          Informe onde as atividades serão realizadas.
+        </div>
+      </div>
+      <div class="col">
+        <label class="form-label">Tipo</label>
+        <select class="form-select" v-model="tipo">
+          <option value="" disabled>Selecione...</option>
+          <option value="1">CLT</option>
+          <option value="2">PJ</option>
+        </select>
+        <div class="form-text">Informe o tipo de contratação.</div>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <button type="submit" class="btn btn-primary" @click="salvarVaga()">
+          Cadastrar
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "PublicarVagaPage",
+  data: () => ({
+    titulo: "",
+    descricao: "",
+    salario: "",
+    modalidade: "",
+    tipo: "",
+  }),
+  methods: {
+    salvarVaga() {
+      let vagas = JSON.parse(localStorage.getItem("vagas"));
+
+      if (!vagas) vagas = [];
+
+      vagas.push({
+        titulo: this.titulo,
+        descricao: this.descricao,
+        salario: this.salario,
+        modalidade: this.modalidade,
+        tipo: this.tipo
+      });
+
+      localStorage.setItem("vagas", JSON.stringify(vagas));
+    },
+  },
 };
 </script>
 
