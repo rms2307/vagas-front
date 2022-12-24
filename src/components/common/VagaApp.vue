cbfa413d1536ee838de64de0d1117dce5a541b36
Robson de Moraes Silva <rms2307@outlook.com>
2022-12-24 11:22:26 -0300

    Criando propriedades computadas para exibição da modalidade e tipo

--- src/components/common/VagaApp.vue
+++ src/components/common/VagaApp.vue
@@ -6,8 +6,8 @@
     </div>
     <div class="card-footer">
       <small class="text-muted">
-        Salário: R$ {{ salario }} | Modalidade: {{ modalidade }} | Tipo:
-        {{ tipo }} | Publicação:
+        Salário: R$ {{ salario }} | Modalidade: {{ getModalidade }} | Tipo:
+        {{ getTipo }} | Publicação:
         {{ publicacao }}
       </small>
     </div>
@@ -66,6 +66,26 @@ export default {
         if (p.length < 5) return false;
         return true;
       },
+    },
+  },
+  computed: {
+    getModalidade() {
+      switch (this.modalidade) {
+        case "1":
+          return "Home Office";
+        case "2":
+          return "Presencial";
+      }
+      return "";
+    },
+    getTipo() {
+      switch (this.tipo) {
+        case "1":
+          return "CLT";
+        case "2":
+          return "PJ";
+      }
+      return "";
     },
   },
 };
